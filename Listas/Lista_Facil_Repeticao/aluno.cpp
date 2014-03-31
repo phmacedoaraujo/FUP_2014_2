a7b6e77c7f8801d12d8e54e50eef79ec60eefca9
phmacedoaraujo <phmacedoaraujo@gmail.com>
2014-03-31 18:23:58 -0300

    Create aluno.cpp

---
+++ Listas/Lista_Facil_Repeticao/aluno.cpp
@@ -0,0 +1,141 @@
+//1 pt
+//Some todos os numeros de 1 a 10..
+int soma_1_a_10()
+{
+    return 0;
+}
+
+//1pt
+//Soma de todos os numeros de ZERO ate o numero inteiro positivo passado
+int somatorio(int num)
+{
+    return 0;
+}
+
+//2pt
+//Faça uma função que retorne quantos números ímpares e divisíveis por 3 existem entre 0 e 500.
+int impares_3_500()
+{
+    return 0;
+}
+
+
+/*1pt
+Uma determinada especie de Bacteria se divide a cada segundo, dizendo que eu
+começo com uma população de apenas 1 bacteria, quantas bacteria eu terei após
+N segundos...
+*/
+int bacteria(int seg)
+{
+    return 0;
+}
+
+/*2pt
+Faça uma função que retorne a soma dos primeiros 200 números naturais que sejam divisíveis
+por 3 mas que não sejam divisíveis por 7.
+*/
+int soma_divisiveis()
+{
+
+}
+
+//1pt
+//Diga se um numero e primo ou nao..
+bool eh_primo(int numero)
+{
+	return 0;
+}
+
+//1pt
+//Diga quantos primos existem entre 0 e num incluindo num.
+int primeiros_primos(int num)
+{
+    return 0;
+}
+
+//1pt
+//Some todos os primos de 1 a N
+int soma_primos(int num)
+{
+    return 0;
+}
+
+/*2pt
+Dado um inteiro N como entrada, determinar no conjunto {1 · · · N} a soma
+de todos os não-primos subtraída da soma dos primos.
+*/
+int nao_primo_menos_primo(int num)
+{
+    return 0;
+}
+
+//2pt
+//Programa que calcula a potencia inteira positiva de um numero
+float potencia(float base, int expoente)
+{
+    return 0;
+}
+
+/*2pt
+Um quadrado perfeito é um número natural cuja raiz quadrada também
+pertence aos naturais. O conjunto dos quadrados perfeitos é {1, 4, 9, 16, 25, 36,49,...}.
+Dado um inteiro de entrada determinar, sem uso de operadores reais
+(como raiz quadrada, por exemplo), se ele é retorne verdadeiro senão retorne falso.
+*/
+bool eh_quadrado_perfeito(int num)
+{
+    return 0;
+}
+
+//2pt
+//Faca um programa que calcule o farorial de um numero
+int fatorial(int numero)
+{
+    return 0;
+}
+
+/*1pt
+Voce deve retornar o enesimo termo da sequencia
+1,2,4,8,16,32,64
+
+Ex: se num for 1, voce retorna 1.
+se num for 4, voce retorna 8.
+Não use potencia, use um laço.
+*/
+int sequencia_1(int num)
+{
+    return 0;
+}
+
+/*2pt
+Voce deve retornar o enesimo termo da sequencia
+1,3,7,15, 31, 63, 127...
+
+Dica: que tal multiplicar o valor acumulado por algo e somar mais outro algo.
+*/
+int sequencia_2(int num)
+{
+    return 0;
+}
+
+/*1pt
+Voce deve retornar o enesimo termo da sequencia
+2, -3, 4, -5, 6, -7, 8, -9 ...
+
+Dica: que tal usar um if
+*/
+int sequencia_3(int num)
+{
+    return 0;
+}
+
+/*3pt
+Voce deve retornar o enesimo termo da sequencia
+3, -6, 15, -42, 123, -366,
+
+Dica: que tal usar tudo que voce aprendeu nas sequencias anteriores.
+*/
+int sequencia_4(int num)
+{
+    return 0;
+}

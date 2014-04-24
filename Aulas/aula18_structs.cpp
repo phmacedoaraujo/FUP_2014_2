2be5408dae92b9aeb12b25c6a429dfc5329cb961
phmacedoaraujo <phmacedoaraujo@gmail.com>
2014-04-24 09:22:09 -0300

    Update aula18_structs.cpp

--- Aulas/aula18_structs.cpp
+++ Aulas/aula18_structs.cpp
@@ -2,18 +2,25 @@
 #include <string.h> // Biblioteca para uso das funções de string em C
 
 using namespace std;
+struct data{
+   int d_nasc;
+   int m_nasc;
+   int a_nasc;
+};
+
+struct tipo_endereco{
+   char rua[20];
+   char CEP[12];
+   char cidade[20];
+};
 
 struct ficha_locadora {
    char nome[20];
    char cpf[11];
    char ddd[3];
    char telefone[8];
-   char rua[20];
-   char CEP[12];
-   char cidade[20];
-   int d_nasc;
-   int m_nasc;
-   int a_nasc;
+   tipo_endereco endereco;
+   data  data_nasc;
 };
 
 //typedef struct ficha_locadora ficha_locadora;
@@ -28,22 +35,22 @@ void ler_dados(ficha_locadora &cliente) {
 	cout << "Digite o telefone do cliente" << endl;
 	cin >> cliente.telefone;
 	cout << "Digite a rua do cliente" << endl;
- 	cin >> cliente.rua;
+ 	cin >> cliente.endereco.rua;
 	cout << "Digite o CEP do cliente" << endl;
-        cin >> cliente.CEP;
+        cin >> cliente.endereco.CEP;
 	cout << "Digite a cidade do cliente" << endl;
-	cin >> cliente.cidade;
+	cin >> cliente.endereco.cidade;
 	cout << "Digite a data de nascimento do cliente: dia, mês e ano" << endl;
-	cin >> cliente.d_nasc;
-	cin >> cliente.m_nasc;
-	cin >> cliente.a_nasc;        
+	cin >> cliente.data_nasc.d_nasc;
+	cin >> cliente.data_nasc.m_nasc;
+	cin >> cliente.data_nasc.a_nasc;        
 }
 
 int main()
 {
    ficha_locadora cliente1;
    ler_dados(cliente1);
-   cout << "Data de nascimento: " << cliente1.d_nasc << "/" << cliente1.m_nasc << "/" << cliente1.a_nasc << endl;
+   cout << "Data de nascimento: " << cliente1.data_nasc.d_nasc << "/" << cliente1.data_nasc.m_nasc << "/" << cliente1.data_nasc.a_nasc << endl;
 
    return 0;
 }

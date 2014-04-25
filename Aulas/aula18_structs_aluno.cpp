dff8e782054b92808679bbed61441c390cc5a3a5
phmacedoaraujo <phmacedoaraujo@gmail.com>
2014-04-25 09:06:51 -0300

    Update aula18_structs_aluno.cpp

--- Aulas/aula18_structs_aluno.cpp
+++ Aulas/aula18_structs_aluno.cpp
@@ -15,8 +15,6 @@ struct aluno_disciplina {
    bool aprovado;
 };
 
-//typedef struct ficha_locadora ficha_locadora;
-
 void ler_dados(aluno_disciplina &aluno) {
         cout << "Digite o nome do aluno" << endl;
 	cin >> aluno.nome;
@@ -34,14 +32,27 @@ void ler_dados(aluno_disciplina &aluno) {
 	else aluno.aprovado = false;       
 }
 
+void imprimir_aluno(aluno_disciplina dados_alunos[], int tam, char matricula[]) {
+  int i;
+  for(i=0;i<tam;i++) { 			
+    if(strcmp(matricula,dados_alunos[i].matricula) == 0) 	{
+	cout << "Disciplina:" << dados_alunos[i].disciplina << endl;
+	cout << "Média: " << dados_alunos[i].media << endl; 
+    }
+   }	
+}
+
 int main()
 {
    int i;
-   aluno_disciplina dados_alunos[5];
-   for(i=0;i<5;i++) {
+   char matricula[8];
+   aluno_disciplina dados_alunos[2];
+   for(i=0;i<2;i++) {
 	ler_dados(dados_alunos[i]);
    }
-
+   cout << "Digite a matrícula a ser psesquisada" << endl;
+   cin >> matricula;
+   imprimir_aluno(dados_alunos,2,matricula);
    return 0;
 }
 

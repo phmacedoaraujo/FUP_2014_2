221839c48aa2c3bdc873fcb7f0e0ba330c161947
phmacedoaraujo <phmacedoaraujo@gmail.com>
2014-05-02 10:00:37 -0300

    Create aluno.cpp

---
+++ Listas/Lista_Facil_Matrizes/aluno.cpp
@@ -0,0 +1,222 @@
+//TITULO   Matrizes
+//BOLSISTA Erivelton Facundo
+
+//HB
+#include <iostream>
+using namespace std;
+//HE
+
+/*S Matrizes S*/
+
+/*Q Preenche matriz Q*/
+
+/*T
+Faça uma função que preenche com 1 cada posição de uma matriz 20x20.
+T*/
+//CB
+void preenche_matriz(int mat[20][20])
+{
+
+}
+//CE
+
+/*Q Preenche matriz um e zero Q*/
+
+/*T
+Faça uma função que crie uma matriz 20x20 e preencha uma linha com 1 e outra linha com  0 e assim sucessivamente.
+T*/
+//CB
+void preenche_matriz_um_e_zero(int mat[20][20])
+{
+
+}
+//CE
+
+/*Q Preenche matriz diagonal principal Q*/
+
+/*T
+Preencha uma matriz 20x20 com 1 na diagonal principal e com 0 os demais elementos.
+T*/
+//CB
+void preenche_matriz_diagonal_principal(int mat[20][20])
+{
+
+}
+//CE
+
+/*Q Maior valor Q*/
+
+/*T
+Receba uma matriz 4x4 e retorne o maior valor.
+T*/
+//CB
+
+int maior_valor_matriz(int matriz[4][4])
+{
+
+	return 0;
+}
+//CE
+
+/*Q Maiores elementos que 10 Q*/
+
+/*T
+Receba uma matriz 4x4, conte e retorne quantos valores maiores que 10 ela possui.
+T*/
+//CB
+int qtd_maiores_elementos(int matriz[4][4])
+{
+
+    return 0;
+}
+//CE
+
+/*Q Menor cade voce Q*/
+
+/*T
+Encontre a posicao e o valor do menor elemento.
+obs:A primeira linha e a primeira coluna possuem índices iguais a zero.
+ T*/
+//CB
+void menor_cade_voce(int mat[4][4], int &linha, int &coluna, int &valor)
+{
+
+}
+//CE
+/*Q Maior cade voce Q*/
+
+/*T
+Encontre a posicao e o valor do maior elemento.
+obs:A primeira linha e a primeira coluna possuem índices iguais a zero.
+ T*/
+//CB
+void maior_cade_voce(int mat[4][4], int &linha, int &coluna, int &valor)
+{
+
+}
+//CE
+
+/*Q Soma todos valores Q*/
+
+/*T
+Somar todos os elementos da matriz 3x3 passada
+ T*/
+//CB
+int somar_todos(int matriz[3][3])
+{
+return 0;
+}
+//CE
+
+/*Q Troca valores Q*/
+
+/*T
+Receba uma matriz 4 x 4 e troque os valores da 1ª.linha pelos da 4ª.coluna, vice-e-versa.
+
+ T*/
+//CB
+void troca_valores(int matriz[4][4])
+{
+
+}
+//CE
+
+/*Q Matriz simetrica Q*/
+
+/*T
+Verifique se uma matriz e simetrica.
+OBS:Lembre que uma matriz M é simétrica é aquela onde M[linha,coluna] = M[coluna,linha].
+ T*/
+//CB
+bool matriz_simetrica(int matriz[3][3])
+{
+return false;
+}
+//CE
+
+/*Q Somar Matriz Q*/
+
+/*T
+Receba duas matrizes M(4,4) e N(4,4) e faça a soma de M com N e coloque o resultado em R.
+ T*/
+//CB
+void soma_matrizes(int M[4][4],int N[4][4],int R[4][4])
+{
+
+}
+//CE
+
+/*Q Subitrair matriz Q*/
+
+/*T
+Receba duas matrizes M(4,4) e N(4,4) e faça a diferenca de M com N e coloque o resultado em R.
+ T*/
+//CB
+void subtraia_matrizes(int M[4][4],int N[4][4],int R[4][4])
+{
+}
+//CE
+
+/*Q Multiplicar Matriz Q*/
+
+/*T
+Receba duas matrizes M(3,3) e N(3,3) e faça a multiplicacao de M com N e coloque o resultado em R.
+ T*/
+//CB
+void multiplica_matrizes(int M[3][3], int N[3][3], int R[3][3])
+{
+
+
+}
+//CE
+
+/*Q Troca matriz por vetor Q*/
+
+/*T
+Receba uma matriz M(3,3) e um valor "a".
+Multiplique a matriz M pelo valor "a" e coloca os valores da matriz multiplicados por A em um vetor V de 9 posições.
+ T*/
+//CB
+void troca_matriz_por_vetor(int M[3][3],int V[9], int a)
+{
+
+}
+//CE
+
+/*Q Troca matriz por vetores somados Q*/
+
+/*T
+Receba uma matriz M(4,4) e preencha 2 vetores L(4) e C(4) que contenham respectivamente:
+As somas das linhas e das colunas de M.
+ T*/
+//CB
+void soma_linha_coluna_matriz(int M[4][4], int L[4],int C[4])
+{
+
+}
+//CE
+/*Q Quadrado magico Q*/
+
+/*T
+Dizemos que uma matriz quadrada inteira é um quadrado magico se a soma dos
+elementos de cada linha, a soma dos elementos de cada coluna e a soma dos
+elementos das diagonais principal e secundária são todas iguais.
+Retorne verdadeiro(true) se a matriz é uma quadrado mágico ou falso(false) caso contrário
+ T*/
+//CB
+bool quadrado_magico(int matriz[3][3])
+{
+    return false;
+}
+//CE
+/*Q Quadrado magico Q*/
+
+/*T
+Bingo - Dado um vetor V(16), verifique quantos numeros voce acertou na matriz(4,4).
+ T*/
+//CB
+int bingo(int V[16], int M[4][4])
+{
+    return 0;
+}
+//CE

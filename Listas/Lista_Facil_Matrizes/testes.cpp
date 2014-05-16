942390affabbf3246a945f842b6d2a4c37b9c882
phmacedoaraujo <phmacedoaraujo@gmail.com>
2014-05-16 08:29:00 -0300

    Update testes.cpp

--- Listas/Lista_Facil_Matrizes/testes.cpp
+++ Listas/Lista_Facil_Matrizes/testes.cpp
@@ -140,24 +140,24 @@ void test_troca_valores()
         int mat[4][4]={{1,2,3,4},{1,1,1,1},{1,1,1,1},{4,3,2,1}};
                 troca_valores(mat);
                 t.add(mat[0][0]==4);
-                t.add(mat[0][1]==1);
-                t.add(mat[0][2]==1);
+                t.add(mat[0][1]==3);
+                t.add(mat[0][2]==2);
                 t.add(mat[0][3]==1);
-                t.add(mat[0][3]==1);
-                t.add(mat[1][3]==2);
-                t.add(mat[2][3]==3);
+                t.add(mat[3][0]==1);
+                t.add(mat[3][1]==2);
+                t.add(mat[3][2]==3);
                 t.add(mat[3][3]==4);
     }
     {
         int mat[4][4]={{1,1,1,1},{1,1,1,1},{1,1,1,1},{0,0,0,0}};
                 troca_valores(mat);
-                t.add(mat[0][0]==1);
-                t.add(mat[0][1]==1);
-                t.add(mat[0][2]==1);
+                t.add(mat[0][0]==0);
+                t.add(mat[0][1]==0);
+                t.add(mat[0][2]==0);
                 t.add(mat[0][3]==0);
-                t.add(mat[0][3]==1);
-                t.add(mat[1][3]==1);
-                t.add(mat[2][3]==1);
+                t.add(mat[3][0]==1);
+                t.add(mat[3][1]==1);
+                t.add(mat[3][2]==1);
                 t.add(mat[3][3]==1);
     }
     t.close();

2be31b25607d49312ba2258c0959ac2311f4cdaf
phmacedoaraujo <phmacedoaraujo@gmail.com>
2014-05-23 09:38:43 -0300

    Create aula22_ponteiros2.cpp

---
+++ Aulas/aula22_ponteiros2.cpp
@@ -0,0 +1,87 @@
+#include <iostream>
+
+using namespace std;
+
+int * busca(int * v, int tam, int x) {
+	int i;
+	for(i=0;i<tam;i++)
+		if(v[i] == x) return &v[i];
+
+	return NULL;
+}
+
+int main()
+{
+   int tam;
+   int *v;
+   int *p;
+   cout << "Digite o tamanho do vetor" << endl;
+   cin >> tam;
+   v = new int[tam];
+   for(int i=0;i<tam;i++) v[i] = i;
+   p = busca(v,tam,2);
+   if(p == NULL) cout << "Elemento não existe no vetor" << endl;
+   else cout << "Endereco = " << p << endl;
+   if(p != NULL) cout << *(p+1) << endl;
+   
+   return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

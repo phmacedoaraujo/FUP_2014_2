d3bd3ed39e2e5dcfdbff3fa2d72fb43ffaffca04
phmacedoaraujo <phmacedoaraujo@gmail.com>
2014-05-29 08:35:10 -0300

    Create aula23_ponteiros4.cpp

---
+++ Aulas/aula23_ponteiros4.cpp
@@ -0,0 +1,110 @@
+#include <iostream>
+
+using namespace std;
+
+struct reg_aluno {
+	int matricula;
+	float media;
+	bool aprovado;
+};
+
+void leitura(reg_aluno *pa1) {
+	cout << "Digite a matricula" << endl;
+	cin >> pa1->matricula;
+	cout << "Digite a média" << endl;
+	cin >> pa1->media;
+	if(pa1->media >= 7.0) pa1->aprovado = true;
+}
+
+void imprime(reg_aluno a1) {
+	cout << "Matrícula: " << a1.matricula << endl;
+        cout << "Média: " << a1.media << endl;
+	if(a1.aprovado) cout << "Aluno aprovado!" << endl;
+        else cout << "Aluno REPROVADO!" << endl; 	
+}
+
+void troca(reg_aluno *pa1, reg_aluno *pa2) {
+	int aux;
+	aux = pa1->matricula;
+	pa1->matricula = pa2->matricula;
+	pa2->matricula = aux;
+
+	float auxf;
+	auxf = pa1->media;
+	pa1->media = pa2->media;
+	pa2->media = auxf;
+
+	bool auxb;
+	auxb = pa1->aprovado;
+	pa1->aprovado = pa2->aprovado;
+	pa2->aprovado = auxb;
+}
+
+int main()
+{
+   reg_aluno a1 = {2,7.5,true};
+   reg_aluno a2 = {3,4.6,false};
+   troca(&a1,&a2);
+   imprime(a1);
+   imprime(a2);
+   return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
